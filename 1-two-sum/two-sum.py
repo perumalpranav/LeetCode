baeef5a1a4702ec906df3decf69a86ece667a0cf
perumalpranav <112510823+perumalpranav@users.noreply.github.com>
2024-03-30 21:54:51 -0400

    Time: 43 ms (62.70%) | Memory: 12.5 MB (78.43%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -7,17 +7,17 @@ class Solution(object):
         """
         sortlist = sorted(nums)
         while(len(sortlist)>1):
-            min = sortlist[0]
+            #min = sortlist[0]
             max = sortlist[len(sortlist)-1]
-            if(min + max > target):
+            if(sortlist[0] + max > target):
                 sortlist.pop(len(sortlist)-1)
-            elif(min + max < target):
+            elif(sortlist[0] + max < target):
                 sortlist.pop(0)
             else:
                 check = [False,False]
                 rlist = []
                 for i in range(len(nums)):
-                    if(nums[i]==min):
+                    if(nums[i]==sortlist[0]):
                         rlist.append(i)
                     elif(nums[i]==max):
                         rlist.append(i)
