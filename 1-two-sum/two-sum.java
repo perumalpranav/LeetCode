f39e0aa8676758cd42543cbd17c0e7393c0942dd
perumalpranav <112510823+perumalpranav@users.noreply.github.com>
2024-03-30 22:17:22 -0400

    Time: 10 ms (53.92%) | Memory: 45.1 MB (9.34%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,15 +1,45 @@
+import java.util.Arrays;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length-1;i++)
+        int[] onums = nums.clone();
+        Arrays.sort(nums);
+        int i = 0;
+        int j = onums.length-1;
+        while(i != j) 
         {
-            for(int j = i+1; j < nums.length;j++)
+            int small = nums[i];
+            int big = nums[j];
+            if(small + big == target) 
             {
-                if((nums[i] + nums[j]) == target){
-                    int sol[] = {i,j};
-                    return sol;
-                }
+                System.out.println(small + " " + big);
+                return origIndices(onums,small,big);
+            }
+            else if(small + big < target)
+            {
+                i++;
+            }
+            else if(small + big > target)
+            {
+                j--;
             }
         }
         return null;
+    }
+
+    public int[] origIndices(int[] array, int num1, int num2) {
+        System.out.println(array[0] + " " + array[1]);
+        int[] sol = {-1,-1};
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] == num1 && sol[0] == -1) {
+                sol[0] = i;
+            }
+            else if(array[i] == num2 && sol[1] == -1) {
+                sol[1] = i;
+            }
+            if(sol[0] != -1 && sol[1] != -1) {
+                break;
+            }
+        }
+        return sol;
     }
 }
