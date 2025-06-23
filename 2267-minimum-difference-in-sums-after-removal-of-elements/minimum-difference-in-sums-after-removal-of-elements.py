db657fefc55190a8777285f6759bc62f58070d72
Pranav Perumal <112510823+perumalpranav@users.noreply.github.com>
2025-06-22 21:55:00 -0700

    Time: 255 ms (91.33%) | Memory: 50.5 MB (39.94%) - LeetSync

--- 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.py
+++ 2267-minimum-difference-in-sums-after-removal-of-elements/minimum-difference-in-sums-after-removal-of-elements.py
@@ -1,14 +1,12 @@
-from heapq import heappush, heappop, heapify
+from heapq import heappushpop, heapify
 
 class Solution:
     def minimumDifference(self, nums: List[int]) -> int:
         #Remove n elements
-        #Split down the middle  minimize(sum(lefthalf) - sum(righthalf))
-
-        #Goal:Minimize Left side, Maximize right side (Ascending order) remove n middle elements
+        #Goal:Minimize Left side, Maximize right side, remove n elements
 
         n = int(len(nums)/3)
-        middle = nums[n:2*n]
+
         maxHeap = [-1 * x for x in nums[:n]]
         minHeap = nums[2*n:]
 
@@ -21,44 +19,22 @@ class Solution:
         #Stores smallest possible prefix with n elements for nums[0...i] where i >= n and i < 2n
         prefixSums = [sum(nums[:n])] #Sum of (inclusive) nums[0:n-1]
         for i in range(n,2*n):
-            tempSum = prefixSums[-1]
-            if i >= n:
-                #Might need to replace an element before calculating new prefix
-                if (-1 * maxHeap[0]) > nums[i]:
-                    #Remove from pastPrefix
-                    removed = -1 * heappop(maxHeap)
-                    tempSum += (nums[i] - removed)
-                    heappush(maxHeap, (-1 * nums[i]))
-            else:
-                tempSum += nums[i]
-
-            prefixSums.append(tempSum)
+            removed = -1 * heappushpop(maxHeap, (-1 * nums[i]))
+            prefixSums.append(prefixSums[-1] - removed + nums[i])
 
         #Stores largest possible suffix with n elements for nums[i...] where i > n and i <= 2n
         suffixSums = [sum(minHeap)] #Sum of (inclusive) nums[2n:3n-1]
-        for i in range((2*n)-1,n-1, -1):
-            tempSum = suffixSums[0]
-            if i >= n:
-                #Might need to replace an element before calculating new suffix
-                if minHeap[0] < nums[i]:
-                    #Remove from pastSuffix
-                    removed = heappop(minHeap)
-                    tempSum += (nums[i] - removed)
-                    heappush(minHeap, nums[i])
-            else:
-                tempSum += nums[i]
-
-            suffixSums.insert(0,tempSum)
+        for i in reversed(range(n,2*n)):
+            removed = heappushpop(minHeap, nums[i])
+            suffixSums.append(suffixSums[-1] - removed + nums[i])
 
 
         #Now for each combination of non-overlapping prefixes and suffixes,
         #Find the one with the minimum difference
         minDifference = float('inf')
-        for p, s in zip(prefixSums, suffixSums):
+        for p, s in zip(prefixSums, suffixSums[::-1]):
             minDifference = min(minDifference, p - s)
 
-        print(prefixSums)
-        print(suffixSums)
         return minDifference
 
 
