df98d3414725bbd55263261e942700f91a6b94a9
Pranav Perumal <112510823+perumalpranav@users.noreply.github.com>
2025-06-27 10:26:09 -0700

    Time: 535 ms (75.41%) | Memory: 18.6 MB (53.01%) - LeetSync

--- 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.py
+++ 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.py
@@ -4,15 +4,47 @@ from functools import reduce
 class Solution:
     def minAnagramLength(self, s: str) -> int:
         def checkT(trial: int) -> bool:
-            sample = sorted(s[0:trial])
-            i = trial
+            i = 0
+            carryOver = None
             while i < len(s):
-                test = sorted(s[i:trial+i])
-                if test != sample:
-                    return False
-                i += trial
-            return True
+                anagramDict = {}
+                for letter in s[i:i+trial]:
+                    if letter not in anagramDict.keys():
+                        anagramDict[letter] = 1
+                    else:
+                        anagramDict[letter] += 1
 
+                print(f"{carryOver} + {anagramDict}")
+
+                for l in letters:
+                    print(l)
+                    if l not in anagramDict.keys():
+                        return False
+                    else:
+                        anagramDict[l] -= 1
+
+                print(f"{carryOver} + {anagramDict}")
+
+                sameLetters = True
+                numLeftOver = 0
+                for key, val in anagramDict.items():
+                    if val != 0:
+                        numLeftOver += val
+                        if carryOver != None:
+                            if carryOver[key] != val:
+                                print(f"{carryOver} + {anagramDict}")
+                                return False
+                        
+                if (len(letters) + numLeftOver != trial) or not sameLetters:
+                    return False
+
+                if carryOver == None:
+                    carryOver = copy.deepcopy(anagramDict)
+
+                i += trial
+
+            return True
+        
         letters = {}
         for c in s:
             letters[c] = letters.setdefault(c,0) + 1
