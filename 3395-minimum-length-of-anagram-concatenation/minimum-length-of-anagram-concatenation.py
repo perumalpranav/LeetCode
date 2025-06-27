3ab3ec8e668dd4abb1764b1f94bb47f40432acfb
Pranav Perumal <112510823+perumalpranav@users.noreply.github.com>
2025-06-27 10:28:04 -0700

    Time: 514 ms (75.96%) | Memory: 19.1 MB (46.99%) - LeetSync

--- 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.py
+++ 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.py
@@ -3,66 +3,33 @@ from functools import reduce
 
 class Solution:
     def minAnagramLength(self, s: str) -> int:
-        def checkT(trial: int) -> bool:
-            i = 0
-            carryOver = None
-            while i < len(s):
-                anagramDict = {}
-                for letter in s[i:i+trial]:
-                    if letter not in anagramDict.keys():
-                        anagramDict[letter] = 1
-                    else:
-                        anagramDict[letter] += 1
-
-                print(f"{carryOver} + {anagramDict}")
-
-                for l in letters:
-                    print(l)
-                    if l not in anagramDict.keys():
-                        return False
-                    else:
-                        anagramDict[l] -= 1
-
-                print(f"{carryOver} + {anagramDict}")
-
-                sameLetters = True
-                numLeftOver = 0
-                for key, val in anagramDict.items():
-                    if val != 0:
-                        numLeftOver += val
-                        if carryOver != None:
-                            if carryOver[key] != val:
-                                print(f"{carryOver} + {anagramDict}")
-                                return False
-                        
-                if (len(letters) + numLeftOver != trial) or not sameLetters:
-                    return False
-
-                if carryOver == None:
-                    carryOver = copy.deepcopy(anagramDict)
-
-                i += trial
-
-            return True
-        
-        letters = {}
+        letters = set()
         for c in s:
-            letters[c] = letters.setdefault(c,0) + 1
+            letters.add(c)
+        
+        #len(letters) minimum possible length of t
 
-        attempt = reduce(gcd,[*letters.values(),len(s)])
-        if len(s) % attempt == 0:
-            attempt = int(len(s)/attempt)
-
-        print(attempt)
-
-        trial = attempt
-        while trial < len(s):
-            if checkT(trial) == True:
-                return trial
-            else:
-                trial += attempt
-
-        return len(s)
-
+        def checkT(trial: int) -> bool:
+            sample = sorted(s[0:trial])
+            i = trial
+            while i < len(s):
+                test = sorted(s[i:trial+i])
+                if test != sample:
+                    return False
+                i += trial
+            return True
 
         
+
+        lo = len(letters)
+        hi = len(s)
+
+        while lo < hi:
+            if len(s) % lo != 0:
+                lo += 1
+                continue
+            if checkT(lo):
+                return lo
+            lo += 1
+
+        return lo
