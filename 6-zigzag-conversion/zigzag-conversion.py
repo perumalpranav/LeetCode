dfc5102c04bcc478db0b670bb16caf250346b62c
Pranav Perumal <112510823+perumalpranav@users.noreply.github.com>
2025-09-07 20:30:44 -0400

    Time: 63 ms (9.37%) | Memory: 18 MB (49.70%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,4 +1,7 @@
-class Solution:
+from collections import deque
+import math
+
+class Solution:  
     def convert(self, s: str, numRows: int) -> str:
         #ith row, jth column
 
@@ -7,67 +10,47 @@ class Solution:
         if n == 1 or n == 2 or numRows == 1:
             return s
 
-        n = len(s)
-
-
         cycleLen = numRows + numRows - 2
-        num_cycles = n//cycleLen + 1
-        col_per_cycle = 1 + numRows - 2
-        num_columns = num_cycles * col_per_cycle
 
-
-        matrix = [['?'] * (num_columns) for _ in range(numRows)]
-
-        previ, prevj = 0, 0
-        i, j = 1, 0
-        
-        matrix[previ][prevj] = s[0]
-        matrix[i][j] = s[1]
-
-        def nextIndex(previ, prevj, i, j):
-            if previ == i - 1:
-                #last direction was downward
-                if i == numRows-1:
-                    #Go diagonally up
-                    return (i-1, j+1)
-                else:
-                    #Go down again
-                    return (i+1, j)
-            elif previ == i + 1 and prevj == j - 1:
-                #last direction was diagonally up
-                if i == 0:
-                    #Go down now
-                    return (i+1, j)
-                else:
-                    #Continue diagonally up
-                    return (i-1, j+1)
-            else:
-                #SOMETHING WENT WRONG
-                print("OHHHHNOOOOOOO")
-
-
-        for c in s[2:]:
-            nexti, nextj = nextIndex(previ, prevj, i, j)
-            matrix[nexti][nextj] = c
-
-
-            previ, prevj = i, j
-            i, j = nexti, nextj
-
+        cycles = [deque(s[i:i+cycleLen]) for i in range(0, n, cycleLen)]
         ans = ""
-        for i in range(numRows):
-            for j in range(num_columns):
-                if matrix[i][j] != '?':
-                    ans = f"{ans}{matrix[i][j]}"
 
-        for m in matrix:
-            for e in m:
-                print(e, end="")
-            print()
+        #Take first character of each cycle
+        for c in cycles:
+            ans = f"{ans}{c.popleft()}"
+
+        #Take first and last character of each full cycle until none are left
+        noneLeft = False
+        passCompleted = 1
+        while not noneLeft:
+            countNone = 0
+            print("Loop Start")
+            for i, c in enumerate(cycles):
+                if not i == len(cycles) - 1:
+                    if len(c) > 0:
+                        ans=f"{ans}{c.popleft()}"
+                    else:
+                        countNone += 1
+                    if len(c) > 0:
+                        ans=f"{ans}{c.pop()}"
+                else:
+                    if len(c) > 0:
+                        ans=f"{ans}{c.popleft()}"
+                    else:
+                        countNone += 1
+                    if len(c) > 0:
+                        #Check if the row is correct
+                        print(f"len: {len(c)} expected: {cycleLen - 2 * passCompleted}")
+
+                        if len(c) == cycleLen - 2 * passCompleted:
+                            ans=f"{ans}{c.pop()}"
+            print("Loop End")
+            passCompleted += 1
+            
+            if countNone == len(cycles):
+                noneLeft = True
 
         return ans
-
-        
 
 
         
