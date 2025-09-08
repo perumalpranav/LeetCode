edba973c692f72998a7f764ba77d2eb54adf7839
Pranav Perumal <112510823+perumalpranav@users.noreply.github.com>
2025-09-07 20:46:36 -0400

    Time: 32 ms (12.10%) | Memory: 18.4 MB (8.13%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -10,47 +10,26 @@ class Solution:
         if n == 1 or n == 2 or numRows == 1:
             return s
 
-        cycleLen = numRows + numRows - 2
+        sub = [[] for _ in range(numRows)]
 
-        cycles = [deque(s[i:i+cycleLen]) for i in range(0, n, cycleLen)]
-        ans = ""
+        rowNum = 0
+        direction = 1
+        for c in s:
+            if rowNum == numRows:
+                direction = -1
+                rowNum = numRows - 2
+            elif rowNum == -1:
+                direction = 1
+                rowNum = 1
 
-        #Take first character of each cycle
-        for c in cycles:
-            ans = f"{ans}{c.popleft()}"
-
-        #Take first and last character of each full cycle until none are left
-        noneLeft = False
-        passCompleted = 1
-        while not noneLeft:
-            countNone = 0
-            print("Loop Start")
-            for i, c in enumerate(cycles):
-                if not i == len(cycles) - 1:
-                    if len(c) > 0:
-                        ans=f"{ans}{c.popleft()}"
-                    else:
-                        countNone += 1
-                    if len(c) > 0:
-                        ans=f"{ans}{c.pop()}"
-                else:
-                    if len(c) > 0:
-                        ans=f"{ans}{c.popleft()}"
-                    else:
-                        countNone += 1
-                    if len(c) > 0:
-                        #Check if the row is correct
-                        print(f"len: {len(c)} expected: {cycleLen - 2 * passCompleted}")
-
-                        if len(c) == cycleLen - 2 * passCompleted:
-                            ans=f"{ans}{c.pop()}"
-            print("Loop End")
-            passCompleted += 1
+            print(rowNum)
+            sub[rowNum].append(c)
+            rowNum += direction
             
-            if countNone == len(cycles):
-                noneLeft = True
+        for i, s in enumerate(sub):
+            sub[i] = "".join(s)
 
-        return ans
+        return "".join(sub)
 
 
         
