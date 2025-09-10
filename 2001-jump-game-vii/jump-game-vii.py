2da8f18e02520b65a5d8f24b5f1ac2a6e73d6223
Pranav Perumal <112510823+perumalpranav@users.noreply.github.com>
2025-09-10 13:18:02 -0400

    Time: 384 ms (7.28%) | Memory: 19.8 MB (70.45%) - LeetSync

--- 2001-jump-game-vii/jump-game-vii.py
+++ 2001-jump-game-vii/jump-game-vii.py
@@ -9,25 +9,27 @@ class Solution:
         if minJump >= n:
             return False
 
-        dq = deque([])
+        num_window = 0
         dsum = 0
         windowlen = maxJump - minJump + 1
 
         for i in range(1,n):
-            if windowlen == len(dq):
-                index = dq.popleft()
-                dsum -= 1 if s[index] == '0' else 0
+            if windowlen == num_window:
+                index = i - maxJump - 1
+                if index >= 0 and index < n:
+                    dsum -= 1 if s[index] == '0' else 0
+                    num_window -= 1
 
                 if i-minJump < n and i-minJump >= 0:
-                    dq.append(i-minJump)
+                    num_window += 1
                     dsum += 1 if s[i-minJump] == '0' else 0
             else:
                 if i-minJump < n and i-minJump >= 0:
-                    dq.append(i-minJump)
+                    num_window += 1
                     dsum += 1 if s[i-minJump] == '0' else 0
             
             #print(f"This {dq}, {dsum}, {i}")
-            if len(dq) > 0 and dsum > 0 and s[i] == '0':
+            if num_window > 0 and dsum > 0 and s[i] == '0':
                 s[i] = '0'
             else:
                 s[i] = '1'
