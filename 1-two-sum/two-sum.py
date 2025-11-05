6ab4eb2a3523173d8d1d614bffe077a6697dba16
Pranav Perumal <112510823+perumalpranav@users.noreply.github.com>
2025-11-05 11:04:24 -0500

    Time: 8 ms (35.29%) | Memory: 21.1 MB (5.78%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,27 +1,18 @@
-class Solution(object):
-   def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        sortlist = sorted(nums)
-        while(len(sortlist)>1):
-            min = sortlist[0]
-            max = sortlist[len(sortlist)-1]
-            if(min + max > target):
-                sortlist.pop(len(sortlist)-1)
-            elif(min + max < target):
-                sortlist.pop(0)
-            else:
-                check = [False,False]
-                rlist = []
-                for i in range(len(nums)):
-                    if(nums[i]==min):
-                        rlist.append(i)
-                    elif(nums[i]==max):
-                        rlist.append(i)
-                    if(len(rlist) == 2):
-                        return rlist
-        
-        return [0,0]
+from collections import defaultdict
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_dict = defaultdict(set)
+        for i, n in enumerate(nums):
+            nums_dict[n].add(i)
+
+        for n in nums_dict.keys():
+            complement = target - n
+            if complement == n:
+                if len(nums_dict[n]) >= 2:
+                    return [nums_dict[n].pop(), nums_dict[n].pop()]
+
+            elif complement in nums_dict.keys():
+                return [nums_dict[n].pop(), nums_dict[complement].pop()]
+
+        return None
