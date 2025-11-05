e8642bd3128b1961099c8528256da32799d4450f
Pranav Perumal <112510823+perumalpranav@users.noreply.github.com>
2025-11-05 17:48:45 -0500

    Time: 7 ms (23.69%) | Memory: 17.8 MB (64.77%) - LeetSync

---
+++ 139-word-break/word-break.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordDict = set(wordDict) #For Hash Lookup
+        n = len(s)
+
+        iList = deque([0])
+        iSet = {0}
+        while len(iList) > 0:
+            print(iList)
+            i = iList.popleft()
+            iSet.remove(i)
+            for j in range(i+1,n+1):
+                if s[i:j] in wordDict:
+                    if j == n:
+                        return True
+                    else:
+                        if j not in iSet:
+                            iList.append(j)
+                            iSet.add(j)
+         
+        return False
