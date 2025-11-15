15dcc1ac27927bc14ba47188e8f9efc31614d9ee
Pranav Perumal <112510823+perumalpranav@users.noreply.github.com>
2025-11-15 18:39:26 -0500

    Time: 4 ms (71.32%) | Memory: 19.3 MB (50.79%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,14 +1,10 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        max = [-1,-1]
-        elements = dict()
-        for e in nums:
-            elements[e] = elements.get(e,0) + 1
-            if(elements.get(e) > max[0]):
-                max[0] = elements.get(e)
-                max[1] = e
-        return max[1]
+from collections import Counter
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        occurences = Counter(nums)
+        for k, v in occurences.items():
+            if v > n//2:
+                return k
+        
