737c22cf9ff8c64d3a62bd4bfb4b2482598786a6
Pranav Perumal <112510823+perumalpranav@users.noreply.github.com>
2026-08-09 10:39:56 -0400

    Time: 7 ms (33.66%) | Memory: 19.1 MB (98.38%) - LeetSync

---
+++ 4052-equal-score-substrings/equal-score-substrings.py
@@ -0,0 +1,20 @@
+class Solution:
+    def scoreBalance(self, s: str) -> bool:
+        alphabet_dict = {
+            'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 
+            'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15, 
+            'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 
+            'w': 23, 'x': 24, 'y': 25, 'z': 26
+        }
+
+        left = 0
+        right = sum(alphabet_dict[c] for c in s)
+
+        for i in range(len(s)):
+            if left == right:
+                return True
+            else:
+                left += alphabet_dict[s[i]]
+                right -= alphabet_dict[s[i]]
+        
+        return False
