baeec7261d51c8f06bc28f71fe9c1fd903c5eadd
Pranav Perumal <112510823+perumalpranav@users.noreply.github.com>
2026-08-16 13:14:49 -0400

    Time: 17 ms (59.62%) | Memory: 126.2 MB (52.88%) - LeetSync

--- 3936-split-array-by-prime-indices/split-array-by-prime-indices.cpp
+++ 3936-split-array-by-prime-indices/split-array-by-prime-indices.cpp
@@ -4,31 +4,32 @@ using namespace std;
 class Solution {
 public:
     long long splitArray(vector<int>& nums) {
-        long long sum = 0, n = nums.size();
-        vector<bool> primes(n, true);
+        vector<bool> primes(nums.size(), true);
 
-        primes[0] = false;
-        if (1 < n) {
-            primes[1] = false;       
-        }
-        
-        for(int i = 2; i * i < n; i += 1) {
-            if(not primes[i]) continue;
-            for(int j = i * i; j < n; j += i) {
-                primes[j] = false;
-            }
-        }
+        long long P = 0;
+        long long notP = 0;
 
         for(int i = 0; i < nums.size(); i++) {
-            if (primes[i]) {
-                sum += nums[i];
+            if (i == 0) {
+                notP += nums[i];
+            }
+            else if (i == 1) {
+                notP += nums[i];
             }
             else {
-                sum -= nums[i];
+                if (primes[i]) {
+                    P += nums[i];
+                    for(int j = i + i; j < nums.size(); j+=i) {
+                        primes[j] = false;
+                    }
+                }
+                else {
+                    notP += nums[i];
+                }
             }
 
         }
 
-        return abs(sum);
+        return abs(P - notP);
     }
 };
